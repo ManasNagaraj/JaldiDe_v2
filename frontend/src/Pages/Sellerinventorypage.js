b5780acd2947725832eb06e90d899c3eae7dd799
brotherlymite <harshsatishpandey@gmail.com>
2020-08-11 19:27:54 +0530

    Making sure that one Seller can Only add one Shop

--- frontend/src/Pages/Sellerinventorypage.js
+++ frontend/src/Pages/Sellerinventorypage.js
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { signin } from '../actions/sellerActions';
- import { saveShop } from '../actions/shopActions';
+ import { saveShop, listShops } from '../actions/shopActions';
 
 export default function Sellerinventorypage(props) {
 
-    const [modalVisible, setModalVisible] = useState(false);
+    let sellerid = props.match.params.id;
+
     const id = props.match.params.id;
     const [name, setName] = useState('');
     const [desc, setDesc] = useState('');
@@ -14,24 +15,37 @@ export default function Sellerinventorypage(props) {
 
     const shopSave = useSelector(state => state.shopSave);
     const { loading: loadingSave, success: successSave, error: errorSave } = shopSave;
+
+    //This is to fetch all the shops
+    const shopList = useSelector(state => state.shopList);
+    const { shops, loading } = shopList;
+
+    let object;
+    if(!loading)
+    {
+        let selleridstring = ""+sellerid;
+        object = shops.find( x => x.seller_id === selleridstring);
+    }
     const dispatch = useDispatch();
 
     useEffect(() => {
+        dispatch(listShops());
         if (successSave) {
             props.history.push("/addproducts/"+id);
-          //setModalVisible(false);
+        }
+        //to check if shop already exsist if so redirect
+        if (object) {
+            if(object._id)
+            {
+                console.log(object._id);
+                props.history.push("/addproducts/"+id);
+            }
         }
         return () => {
           //
         };
-      }, [successSave]);
+      }, [successSave,loading]);
 
-    const openModal = (shop) => {
-        setModalVisible(true);
-        setName(shop.name);
-        setDesc(shop.desc);
-        setCategory(shop.category);
-    }
 
     const submitHandler = (e) => {
         e.preventDefault();
@@ -45,11 +59,6 @@ export default function Sellerinventorypage(props) {
         
         <div className="content content-margined">
 
-            <div className="product-header">
-            <button className="button primary" onClick={() => openModal({})}>Create your Virtual Shop</button>
-            </div>
-            {
-            modalVisible &&
             <div className="form">
                 <form onSubmit={submitHandler} >
                 <ul className="form-container">
@@ -87,18 +96,11 @@ export default function Sellerinventorypage(props) {
 
                     <button type="submit" className="button primary">Create Shop</button>
                     </li>
-                    <li>
-                    <button type="button" onClick={() => setModalVisible(false)} className="button secondary">Back</button>
-                    </li>
+                    
                 </ul>
                 </form>
             </div>
-            }
+            
     </div>
     )
 }
-
-{/* <div>
-<h3>Wassup new Seller, you can make your own virtual shop here</h3>
-<h2>{props.match.params.id}</h2>
-</div> */}
