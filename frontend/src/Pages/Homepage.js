3c607b05f56780bbf219abc327685c8357c5e3eb
omkark45 <omkarak45@gmail.com>
2020-08-15 22:44:45 +0530

    Improved landing page.
    1. Added CSS for homepage.js to index.css
    2. Commented out unncessary h1 styling from Shoppage CSS.
    3. Added hero pattern image.
    4. Added two call to action buttons (explore and login).

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -3,6 +3,10 @@ import Shops from '../components/Shops';
 import { useSelector, useDispatch } from 'react-redux';
 import { listShops } from '../actions/shopActions';
 
+// Carousel
+import Carousel from 'react-bootstrap/Carousel'
+
+
 export default function Homepage() {
     
     const shopList = useSelector(state => state.shopList);
@@ -20,6 +24,27 @@ export default function Homepage() {
     return (
         loading ? <div>loading...</div> : error ? {error} :
         <div>
+            {/* html goes here */}
+            <div className='d-flex height-80 justify-content-center align-items-center flex-column'>
+            <h1 className='hero-title text-center mb-3'>
+                Groceries delivered fast.
+            </h1>
+            <p className='lead text-center mb-5'>loralksdfklancnaksj dnffjasdhdfkj f afdksjahfkajsdhfkjas askjdfhakjshdf ksjhf asfkjsddfh jkfhdsjk hfasdf asdfhsajkdfh jkshf ewuhs dhashf shfkajshfh</p>
+
+            <div className='d-flex mb-3'>
+
+            <a className='btn  hero-btn'>Explore</a>
+            <a className='btn hero-btn login-btn'>Login</a>
+            </div>
+
+
+
+            </div>
+
+
+
+
+
             <Shops shops={shops}></Shops>
         </div>
     )
