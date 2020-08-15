fce1861c585a581eb6155246c665bf0463ae469c
Sushil Buragute <50674860+sushilburagute@users.noreply.github.com>
2020-08-15 15:28:14 +0530

    Added Hero

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,26 +1,33 @@
-import React, { useEffect } from 'react'
-import Shops from '../components/Shops';
-import { useSelector, useDispatch } from 'react-redux';
-import { listShops } from '../actions/shopActions';
+import React, { useEffect } from "react";
+import Shops from "../components/Shops";
+import { useSelector, useDispatch } from "react-redux";
+import { listShops } from "../actions/shopActions";
 
 export default function Homepage() {
-    
-    const shopList = useSelector(state => state.shopList);
-    const { shops, loading, error } = shopList;
-    const dispatch = useDispatch();
+  const shopList = useSelector((state) => state.shopList);
+  const { shops, loading, error } = shopList;
+  const dispatch = useDispatch();
 
-    useEffect(() => 
-    {
-        dispatch(listShops());
-        return () => {
-            //
-        };
-    }, [])
+  useEffect(() => {
+    dispatch(listShops());
+    return () => {
+      //
+    };
+  }, []);
 
-    return (
-        loading ? <div>loading...</div> : error ? {error} :
-        <div>
-            <Shops shops={shops}></Shops>
-        </div>
-    )
+  return loading ? (
+    <div>loading...</div>
+  ) : error ? (
+    { error }
+  ) : (
+    <div>
+      <div>
+        <h1>Hero</h1>
+      </div>
+      <div>
+        <h1>How to Use</h1>
+      </div>
+      <Shops shops={shops}></Shops>
+    </div>
+  );
 }
