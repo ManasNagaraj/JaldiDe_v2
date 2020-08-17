c7ca836bc508ca5fb42d20fdcd4171cb63612935
ManasNagaraj <48147533+ManasNagaraj@users.noreply.github.com>
2020-08-17 11:48:53 +0530

    some error handling

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -9,20 +9,25 @@ const router = express.Router();
 // @desc     User Signin
 // @access   Private
 router.post('/signin', async (req, res) => {
-  const signinUser = await User.findOne({
-    email: req.body.email,
-    password: req.body.password,
-  });
-
-  if (signinUser) {
-    res.send({
-      _id: signinUser.id,
-      name: signinUser.name,
-      email: signinUser.email,
-      token: getToken(signinUser),
+  try {
+    const signinUser = await User.findOne({
+      email: req.body.email,
+      password: req.body.password,
     });
-  } else {
-    res.status(401).send({ msg: 'Invalid Email or Password.' });
+
+    if (signinUser) {
+      res.send({
+        _id: signinUser.id,
+        name: signinUser.name,
+        email: signinUser.email,
+        token: getToken(signinUser),
+      });
+    } else {
+      res.status(401).send({ msg: 'Invalid Email or Password.' });
+    }
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('server error');
   }
 });
 
@@ -30,21 +35,26 @@ router.post('/signin', async (req, res) => {
 // @desc     User Register
 // @access   Private
 router.post('/register', async (req, res) => {
-  const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  });
-  const newUser = await user.save();
-  if (newUser) {
-    res.send({
-      _id: newUser.id,
-      name: newUser.name,
-      email: newUser.email,
-      token: getToken(newUser),
+  try {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
     });
-  } else {
-    res.status(401).send({ msg: 'Invalid User Data.' });
+    const newUser = await user.save();
+    if (newUser) {
+      res.send({
+        _id: newUser.id,
+        name: newUser.name,
+        email: newUser.email,
+        token: getToken(newUser),
+      });
+    } else {
+      res.status(401).send({ msg: 'Invalid User Data.' });
+    }
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('server error');
   }
 });
 
@@ -54,12 +64,17 @@ router.post('/register', async (req, res) => {
 // @desc     Create an order
 // @access   Private
 router.get('/stuff1', async (req, res) => {
-  const shop = await Shop.findOne({
-    _id: '5f2aff9027c38862cc6bbfd0',
-  });
-  if (shop) {
-    shop.products.push({ pname: 'aa 2', pdesc: 'aaaa 2' });
-    res.send(shop);
+  try {
+    const shop = await Shop.findOne({
+      _id: '5f2aff9027c38862cc6bbfd0',
+    });
+    if (shop) {
+      shop.products.push({ pname: 'aa 2', pdesc: 'aaaa 2' });
+      res.send(shop);
+    }
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('server error');
   }
 });
 
